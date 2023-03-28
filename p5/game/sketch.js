a4ecd1c9b627f8cd8d0a6bb873ad002ed151bf29
paolapetitti123 <44977432+paolapetitti123@users.noreply.github.com>
2023-03-28 14:42:49 -0400

    game

--- p5/game/sketch.js
+++ p5/game/sketch.js
@@ -1,8 +1,8 @@
 // Variable Declarations
 let canvas;
+let screen = 0;
 
 function preload(){
-
 }
 
 function setup() {
@@ -13,4 +13,50 @@ function setup() {
 function draw() {
   // put drawing code here
   background(0,0,0);
+  if(screen == 0){
+    menuScreen();
+  }
+  else if(screen == 1)
+  {
+    gameScreen();
+  }
+  else if(screen == 2){
+    gameOverScreen();
+  }
+}
+
+function menuScreen(){
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(width / 10);
+  fill(255,255,255);
+  textFont("VT323");
+  text('ASSESMENT TEST',width/2,110);
+  pop();
+  
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(width / 15);
+  fill(255,255,255);
+  textFont("VT323");
+  text('Start', width/2.7, 200);
+  pop();
+
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(width / 15);
+  fill(255,255,255);
+  textFont("VT323");
+  text('Quit', width/1.6, 200);
+  pop();
+
+  
+}
+
+function gameScreen(){
+
+}
+
+function gameOverScreen(){
+
 }
