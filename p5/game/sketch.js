d314157da7cbd928c431dbe2880c355e8c8df4e2
paolapetitti123 <44977432+paolapetitti123@users.noreply.github.com>
2023-03-28 12:41:53 -0400

    testing to see if this works in my game

---
+++ p5/game/sketch.js
@@ -0,0 +1,16 @@
+// Variable Declarations
+let canvas;
+
+function preload(){
+
+}
+
+function setup() {
+  // put setup code here
+  canvas = createCanvas(1080, 1080);
+}
+
+function draw() {
+  // put drawing code here
+  background(0,0,0);
+}
