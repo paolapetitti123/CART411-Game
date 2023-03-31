3ca4fb555ec9536f4267c6ad0f4b4a804b897b48
paolapetitti123 <44977432+paolapetitti123@users.noreply.github.com>
2023-03-30 23:03:17 -0400

    added welcome message and new scene

--- p5/game/sketch.js
+++ p5/game/sketch.js
@@ -5,20 +5,28 @@ let startTextImg;
 let quitTextImg;
 let startImg;
 let quitImg;
+let contImg;
 let vid;
 let vidPlaying = true;
 let timer = 6;
+let welcomeMessage;
+let messagePlayCount = 0;
+let tvStaticSound;
 
 
 function preload(){
   startImg = createImg('./pictures/start.png');
   quitImg = createImg('./pictures/quit.png');
+  contImg = createImg('./pictures/continue.png');
   vid = createVideo(['./video/newworld_LogoSmall.mp4'],vidSettings);
+  welcomeMessage = loadSound('./audio/welcome_message.mp3');
+  tvStaticSound = loadSound('./audio/tv_static.wav');
 }
 
 function setup() {
   // put setup code here
   canvas = createCanvas(430, 300);
+  
   vid.hide();
 }
 
@@ -33,11 +41,18 @@ function draw() {
     gameScreen();
   }
   else if(screen == 2){
+    captchaStart();
+  }
+  else if(screen == 3){
     gameOverScreen();
   }
 }
 
 function menuScreen(){
+  if(!tvStaticSound.isPlaying()){
+    tvStaticSound.loop();
+    tvStaticSound.setVolume(0.6);
+  }
   push();
   textAlign(CENTER, CENTER);
   textSize(width / 10);
@@ -56,7 +71,7 @@ function menuScreen(){
 
 function gameScreen(){
   vidLoad();
- 
+  
 }
 
 function gameOverScreen(){
@@ -70,6 +85,8 @@ function vidSettings(){
 }
 
 function vidLoad(){
+  startImg.hide();
+  quitImg.hide();
   image(vid,10,10);
   if(frameCount % 60 == 0 && timer > 0){
     vid.play();
@@ -79,19 +96,51 @@ function vidLoad(){
   {
     vid.stop();
     vid.size(0,0);
-
-    push();
-    textAlign(CENTER, CENTER);
-    textSize(width / 10);
-    fill(255,255,255);
-    textFont("VT323");
-    text('TEST',width/2,110);
-    pop();
+    // removeElements();
+    introGame(); 
+    
   }
-  
+}
+
+function introGame()
+{ 
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(width / 15);
+  fill(255,255,255);
+  textFont("VT323");
+  text(`Hello and welcome to\nNew World Corporation.`,width/2,110);
+  pop();
+
+
+  if(!welcomeMessage.isPlaying() && messagePlayCount <= 1){
+    welcomeMessage.play();
+    messagePlayCount++;
+    console.log("Counter:" + messagePlayCount);
+  }
+  else if(messagePlayCount == 2){
+    welcomeMessage.stop();
+    contImg.position(canvas.width / 3.01, canvas.height /1.5);
+    contImg.mouseClicked(changeScene);
+  }
+
+
+}
+
+function captchaStart(){
+  contImg.hide();
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(width / 15);
+  fill(255,255,255);
+  textFont("VT323");
+  text(`TESTING.`,width/2,110);
+  pop();
 }
 
 function changeScene(){
-  removeElements();
+  // removeElements();
   screen += 1;
+
 }
+
